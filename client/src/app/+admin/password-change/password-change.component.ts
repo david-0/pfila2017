fa11d8112500f6b8090d0397817692d138cb3a10
David Leuenberger <david.leuenberger@hsr.ch>
2017-02-27 21:36:44 +0100

    remove formGroup and formControl form code

--- client/src/app/+admin/password-change/password-change.component.ts
+++ client/src/app/+admin/password-change/password-change.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms";
+import {Component, OnInit} from "@angular/core";
+import {FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
-import {AuthGuard} from "../services/auth-guard.service";
 import {AuthenticationService} from "../services/authentication.service";
 import {GenericRestService} from "../../remote/generic-rest.service";
 import {IUser} from "../../../../../server/entities/user.interface";
@@ -13,57 +12,42 @@ import {AuthHttp} from "angular2-jwt";
   styleUrls: ['./password-change.component.scss']
 })
 export class PasswordChangeComponent implements OnInit {
-
-  private form: FormGroup;
-  private currentPasswordControl: FormControl;
-  private passwordControl: FormControl;
-  private passwordRetypeControl: FormControl;
   private message: string;
   private restService: GenericRestService<IUser>;
 
-  constructor(private fb: FormBuilder,
-              private authenticationService: AuthenticationService,
-              private authGuard: AuthGuard,
+  constructor(private authenticationService: AuthenticationService,
               private router: Router,
               private http: AuthHttp) {
-    this.currentPasswordControl = new FormControl('', [Validators.required]);
-    this.passwordControl = new FormControl('', [Validators.required, Validators.minLength(7)]);
-    this.passwordRetypeControl = new FormControl('', [Validators.minLength(7)]); // equals
-    this.form = fb.group({
-      "currentPassword": this.currentPasswordControl,
-      "password": this.passwordControl,
-      "passwordRetype": this.passwordRetypeControl,
-    });
   }
 
-  private changePassword(data: any, valid: boolean): void {
+  private changePassword(data: any, form: FormGroup,): void {
     this.authenticationService.loginCheckonly(this.authenticationService.getLoggedInEmail(), data.currentPassword)
       .subscribe(result => {
-          if (result === true) {
-            this.restService.get(this.authenticationService.getLoggedInUserId()).subscribe(
-              user => {
-                user.password = data.password;
-                this.restService.update(user).subscribe(
-                  user => {
-                    this.message = 'Passwort geändert';
-                    this.form.reset();
-                    setTimeout(() => this.router.navigate(['/admin/dashboard']), 2000);
-                  }
-                  , error => {
-                    this.message = `Fehler beim Passwot ändern (${error})`;
-                  }
-                );
-              },
-              error => {
-                this.message = `Fehler beim Passwort ändern (${error})`
-              });
-          } else {
-            this.message = 'Ungültige Anwort vom Server. Server-Fehler.';
-          }
-        }, error => {
-          this.message = `Aktuelles Passwort nicht korrekt (${error})`;
+        if (result === true) {
+          this.updatePassword(data.password, form);
+        } else {
+          this.message = 'Ungültige Anwort vom Server. Interner Server-Fehler.';
         }
-      );
+      }, error => {
+        this.message = `Aktuelles Passwort nicht korrekt (${error})`;
+      });
+  }
+
+  private updatePassword(password: string, form: FormGroup) {
+    this.restService.get(this.authenticationService.getLoggedInUserId()).subscribe(
+      user => {
+        user.password = password;
+        this.restService.update(user).subscribe(user => {
+          this.message = 'Passwort geändert';
+          form.reset();
+          setTimeout(() => this.router.navigate(['/admin/dashboard']), 2000);
+        }, error => {
+          this.message = `Fehler beim Passwot ändern (${error})`;
+        });
+      },
+      error => {
+        this.message = `Fehler beim Passwort ändern (${error})`
+      });
   }
 
   ngOnInit() {
