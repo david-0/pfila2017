899823c378ba86ed3757751b4dc60d328d331144
David Leuenberger <david.leuenberger@hsr.ch>
2017-03-06 22:47:22 +0100

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ const gulpExe = require.resolve('./node_modules/.bin/gulp');
 
 gulp.task('default', ['run-without-client-logs']);
 gulp.task('run-with-client-logs', ['watch-and-build-client', 'run-server']);
-gulp.task('run-without-client-logs', ['watch-and-build-client-without-logs', 'run-server']);
+//gulp.task('run-without-client-logs', ['watch-and-build-client-without-logs', 'run-server']);
 gulp.task('run-server', function () {
     spawn(gulpExe, [], {stdio: 'inherit', cwd: 'server', shell: true});
 });
 gulp.task('watch-and-build-client', function () {
     spawn(gulpExe, [], {stdio: 'inherit', cwd: 'client', shell: true});
 });
-gulp.task('watch-and-build-client-without-logs', function () {
-    spawn(gulpExe, ['watch-and-build-without-logs'], {stdio: 'inherit', cwd: 'client', shell: true});
-});
+//gulp.task('watch-and-build-client-without-logs', function () {
+//    spawn(gulpExe, ['watch-and-build-without-logs'], {stdio: 'inherit', cwd: 'client', shell: true});
+//});
 
 gulp.task('install', ['npm-install-server', 'npm-install-client']);
 gulp.task('npm-install-server', function () {
